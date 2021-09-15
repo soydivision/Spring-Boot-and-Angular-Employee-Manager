6d29463e83c2973b0139f33567a362c46d07c5a0
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-09-15 16:15:23 +0600

    genesis

---
+++ src/main/java/com/github/passmesomesugar/employee_management_system/model/Employee.java
@@ -0,0 +1,20 @@
+package com.github.passmesomesugar.employee_management_system.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private String id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+}
