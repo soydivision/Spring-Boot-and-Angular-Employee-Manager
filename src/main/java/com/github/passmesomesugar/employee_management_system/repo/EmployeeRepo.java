ea0179f734444dffc69e89f30fd5ab3bdecbbb79
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-09-22 17:20:36 +0600

    ++Optional, methods

--- src/main/java/com/github/passmesomesugar/employee_management_system/repo/EmployeeRepo.java
+++ src/main/java/com/github/passmesomesugar/employee_management_system/repo/EmployeeRepo.java
@@ -3,9 +3,10 @@ package com.github.passmesomesugar.employee_management_system.repo;
 import com.github.passmesomesugar.employee_management_system.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
 
-
-
+    Optional findEmployeeById(Long id);
 }
--- src/main/java/com/github/passmesomesugar/employee_management_system/service/EmployeeService.java
+++ src/main/java/com/github/passmesomesugar/employee_management_system/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.github.passmesomesugar.employee_management_system.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -21,5 +22,22 @@ public class EmployeeService {
         return employeeRepo.save(employee);
     }
 
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User was not found,id: " + id));
+
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
 
 }
