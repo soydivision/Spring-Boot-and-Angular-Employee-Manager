a7efbf2acc82867d2591e257de1180708b88e794
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-09-24 16:02:16 +0600

    Api works

---
+++ employeemanagerapp/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee'
+import { EmployeeService } from './employee.service'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+   public employees: Employee[];
+
+   constructor(private employeeService: EmployeeService){}
+   
+  ngOnInit()
+  {
+    this.getEmployeees();
+  }
+
+   public getEmployeees(): void {
+    this.employeeService.getEmployees().subscribe(
+    (response: Employee[]) => {this.employees= response;
+    },
+    (error: HttpErrorResponse) => {alert(error.message); 
+    });
+   }
+}
